2368bfdab9cf901f253951ed601a3ddfd00dbe6b
RyanTheMagic <ryanlucas.dev@gmail.com>
2026-07-14 23:24:47 -0300

    Expressões Aritmeticas

---
+++ EmCurso/expressoes_aritmeticas/src/Program.java
@@ -0,0 +1,26 @@
+//Seção 4 - Aula 28
+
+public class Program {
+	public static void main (String[]args) {
+		//Ordem Mátematica
+		
+		System.out.println("2 * 6 / 3 = "+ (2 * 6 / 3));
+		//Resultado: 4
+		System.out.println("3 + 2 * 4 = "+ (3 + 2 * 4));
+		//Resultado: 11
+		System.out.println("(3 + 2) * 4 = "+ (3 + 2) * 4);
+		//Resultado: 20
+		System.out.println("60/(3 + 2) * 4 = "+ 60/(3 + 2) * 4);
+		//Resultado: 48
+		System.out.println("60/((3 + 2) * 4) = "+ 60/((3 + 2) * 4));
+		//Resultado: 3
+		
+		//Operador "Mod" (%)
+		
+		System.out.println("14 % 3 = "+ 14 % 3);
+		//Resultado: 2
+		System.out.println("19 % 5 = "+ 19 % 5);
+		//Resultado: 4
+		
+	}
+}
