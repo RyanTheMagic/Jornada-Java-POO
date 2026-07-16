afe133abf21a01d3a419038571bdc9f7a14ce880
RyanTheMagic <ryanlucas.dev@gmail.com>
2026-07-16 00:27:59 -0300

    Exibindo, Declarando e Formatando

---
+++ EmCurso/saida_de_dados/src/Program.java
@@ -0,0 +1,24 @@
+//Seção 4 - Aula 31
+
+public class Program {
+
+	public static void main(String[] args) {
+		//Sem quebra de Linha
+		System.out.print("---");
+		System.out.print("Hello World");
+		System.out.print("---");
+		
+		System.out.println();
+		
+		//Com quebra de linha
+		System.out.println("---");
+		System.out.println("Hello World");
+		System.out.println("---");
+		
+		//Declarando variável
+		int idade = 32;
+		
+		System.out.println("Idade: " + idade + " anos");
+	}
+
+}
