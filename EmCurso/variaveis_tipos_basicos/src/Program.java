0ffa5d9aaaaa66cf6db9573ab0db53aabdb9170b
RyanTheMagic <ryanlucas.dev@gmail.com>
2026-07-16 00:27:11 -0300

    Tipos Primitivos

---
+++ EmCurso/variaveis_tipos_basicos/src/Program.java
@@ -0,0 +1,20 @@
+
+//Seção 4 - Aula 29
+
+public class Program {
+	
+	public static void main(String[] args) {
+		//Sintaxe: <tipo> <nome> = <valor inicial>
+		
+		int idade = 25;
+		double altura = 1.68;
+		char genero = 'F';
+		
+		System.out.println("????");
+		System.out.println("Minha idade é: "+ idade);
+		System.out.println("Minha altura é: "+ altura);
+		System.out.println("Meu género é: "+ genero);
+		
+	}
+
+}
